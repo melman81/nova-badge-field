269dcec622ee65f920566061b7c7b375ac8051f1
melman30 <mhellings@qdservices.nl>
2022-07-28 16:17:44 +0200

    Update IndexField to center the badge in the table

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,16 +1,18 @@
 <template>
+  <div class="text-center">
     <span class="rounded-full uppercase px-2 py-1 text-xs font-bold whitespace-no-wrap"
-        :class="badgeExtraClasses"
-        :style="{ backgroundColor: backgroundColor(), color: textColor() }"
+          :class="badgeExtraClasses"
+          :style="{ backgroundColor: backgroundColor(), color: textColor() }"
     >{{ __(displayValue()) }}</span>
+  </div>
 </template>
 
 <script>
-    import colors from '../mixins/colors';
-    import display from '../mixins/display';
+import colors from '../mixins/colors';
+import display from '../mixins/display';
 
-    export default {
-        props: ['resourceName', 'field'],
-        mixins: [colors, display]
-    }
+export default {
+  props: ['resourceName', 'field'],
+  mixins: [colors, display]
+}
 </script>
